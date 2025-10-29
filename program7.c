b7d52ef6722125cc3461386e0f9d3852288db7c1
hiteshbalaji147-rgb <hiteshbalaji147@gmail.com>
2025-10-29 19:17:59 +0530

    Add program7.c - Part 2: Input function for process details

--- program7.c
+++ program7.c
@@ -11,3 +11,23 @@ struct Process {
     int turnaround_time;  // Turnaround time
     bool completed;       // Completion status
 };
+
+// Function to input process details
+void inputProcesses(struct Process proc[], int n) {
+    printf("Shortest Job First (SJF) Scheduling Algorithm\n");
+    printf("==============================================\n\n");
+    
+    for (int i = 0; i < n; i++) {
+        proc[i].pid = i + 1;
+        printf("Process P%d:\n", i + 1);
+        printf("  Arrival Time: ");
+        scanf("%d", &proc[i].arrival_time);
+        printf("  Burst Time: ");
+        scanf("%d", &proc[i].burst_time);
+        proc[i].completed = false;
+        proc[i].completion_time = 0;
+        proc[i].waiting_time = 0;
+        proc[i].turnaround_time = 0;
+        printf("\n");
+    }
+}
