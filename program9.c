f4006c6c95155068953181f9db57aaa2846e360a
hiteshbalaji147-rgb <hiteshbalaji147@gmail.com>
2025-11-01 10:42:52 +0530

    Add program9.c - Part 1: Header files and queue constants

---
+++ program9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Constants
+#define MAX_PROCESSES 20
+#define MAX_QUEUES 3
+#define SYSTEM_QUEUE 0
+#define USER_QUEUE 1
+#define BATCH_QUEUE 2
